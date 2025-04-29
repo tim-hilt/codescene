4c3966bc0d226143dcca0ee1355afde116364eda
Tim Hilt <timhilt@live.de>
2025-04-29 17:35:02 +0200

    buggy first version with scc + git

---
+++ go.mod
@@ -0,0 +1,65 @@
+module github.com/tim-hilt/codescene
+
+go 1.24.2
+
+require (
+	github.com/go-git/go-git/v5 v5.16.0
+	github.com/marcboeker/go-duckdb/v2 v2.2.0
+)
+
+require (
+	dario.cat/mergo v1.0.1 // indirect
+	github.com/Microsoft/go-winio v0.6.2 // indirect
+	github.com/ProtonMail/go-crypto v1.2.0 // indirect
+	github.com/apache/arrow-go/v18 v18.1.0 // indirect
+	github.com/boyter/gocodewalker v1.4.0 // indirect
+	github.com/boyter/scc/v3 v3.5.0 // indirect
+	github.com/cloudflare/circl v1.6.1 // indirect
+	github.com/cyphar/filepath-securejoin v0.4.1 // indirect
+	github.com/danwakefield/fnmatch v0.0.0-20160403171240-cbb64ac3d964 // indirect
+	github.com/duckdb/duckdb-go-bindings v0.1.14 // indirect
+	github.com/duckdb/duckdb-go-bindings/darwin-amd64 v0.1.9 // indirect
+	github.com/duckdb/duckdb-go-bindings/darwin-arm64 v0.1.9 // indirect
+	github.com/duckdb/duckdb-go-bindings/linux-amd64 v0.1.9 // indirect
+	github.com/duckdb/duckdb-go-bindings/linux-arm64 v0.1.9 // indirect
+	github.com/duckdb/duckdb-go-bindings/windows-amd64 v0.1.9 // indirect
+	github.com/emirpasic/gods v1.18.1 // indirect
+	github.com/go-git/gcfg v1.5.1-0.20230307220236-3a3c6141e376 // indirect
+	github.com/go-git/go-billy/v5 v5.6.2 // indirect
+	github.com/go-viper/mapstructure/v2 v2.2.1 // indirect
+	github.com/goccy/go-json v0.10.5 // indirect
+	github.com/golang/groupcache v0.0.0-20241129210726-2c02b8208cf8 // indirect
+	github.com/google/flatbuffers v25.1.24+incompatible // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/jbenet/go-context v0.0.0-20150711004518-d14ea06fba99 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/kevinburke/ssh_config v1.2.0 // indirect
+	github.com/klauspost/compress v1.17.11 // indirect
+	github.com/klauspost/cpuid/v2 v2.2.9 // indirect
+	github.com/marcboeker/go-duckdb/arrowmapping v0.0.7 // indirect
+	github.com/marcboeker/go-duckdb/mapping v0.0.7 // indirect
+	github.com/mattn/go-runewidth v0.0.16 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/pierrec/lz4/v4 v4.1.22 // indirect
+	github.com/pjbgf/sha1cd v0.3.2 // indirect
+	github.com/rivo/uniseg v0.4.7 // indirect
+	github.com/sergi/go-diff v1.3.2-0.20230802210424-5b0b94c5c0d3 // indirect
+	github.com/skeema/knownhosts v1.3.1 // indirect
+	github.com/spf13/cobra v1.8.1 // indirect
+	github.com/spf13/pflag v1.0.6 // indirect
+	github.com/xanzy/ssh-agent v0.3.3 // indirect
+	github.com/zeebo/xxh3 v1.0.2 // indirect
+	golang.org/x/crypto v0.37.0 // indirect
+	golang.org/x/exp v0.0.0-20250128182459-e0ece0dbea4c // indirect
+	golang.org/x/mod v0.22.0 // indirect
+	golang.org/x/net v0.39.0 // indirect
+	golang.org/x/sync v0.13.0 // indirect
+	golang.org/x/sys v0.32.0 // indirect
+	golang.org/x/text v0.24.0 // indirect
+	golang.org/x/tools v0.29.0 // indirect
+	golang.org/x/xerrors v0.0.0-20240903120638-7835f813f4da // indirect
+	gopkg.in/warnings.v0 v0.1.2 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+)
---
+++ internal/analyze.go
@@ -0,0 +1,270 @@
+package internal
+
+import (
+	"database/sql"
+	"fmt"
+	"io/fs"
+	"os"
+	"runtime"
+	"strings"
+	"sync"
+
+	"github.com/boyter/scc/v3/processor"
+	"github.com/go-git/go-billy/v5"
+	"github.com/go-git/go-billy/v5/memfs"
+	"github.com/go-git/go-billy/v5/util"
+	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/plumbing/object"
+	"github.com/go-git/go-git/v5/storage/memory"
+
+	// TODO: Try to get arrow support working
+	_ "github.com/marcboeker/go-duckdb/v2"
+)
+
+func initDB() (*sql.DB, error) {
+	db, err := sql.Open("duckdb", "codescene.db")
+	if err != nil {
+		return nil, err
+	}
+
+	createTablesStmt := `
+				CREATE TABLE IF NOT EXISTS commits (
+					hash TEXT PRIMARY KEY UNIQUE,
+					contributor TEXT NOT NULL,
+					author_date INTEGER NOT NULL,
+					project TEXT NOT NULL
+				);
+				CREATE TABLE IF NOT EXISTS filestates (
+					id INTEGER PRIMARY KEY,
+					commit_hash TEXT NOT NULL REFERENCES commits(hash),
+					path TEXT NOT NULL,
+					language TEXT NOT NULL,
+					sloc INTEGER NOT NULL,
+					cloc INTEGER NOT NULL,
+					blank INTEGER NOT NULL,
+					lines_added INTEGER NOT NULL,
+					lines_removed INTEGER NOT NULL,
+					complexity INTEGER NOT NULL,
+					blame_authors TEXT[] NOT NULL,
+					blame_dates INTEGER[] NOT NULL
+				);` // TODO: Add array for blame
+	_, err = db.Exec(createTablesStmt)
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
+
+type FileState struct {
+	CommitId int64
+	Commit   *object.Commit
+	*processor.FileJob
+}
+
+func Analyze() error {
+	db, err := initDB()
+	if err != nil {
+		return err
+	}
+	defer db.Close()
+
+	repoUrl := "https://github.com/go-git/go-billy"
+	filesystem := memfs.New()
+	r, err := git.Clone(memory.NewStorage(), filesystem, &git.CloneOptions{
+		URL: repoUrl,
+	})
+
+	if err != nil {
+		return err
+	}
+
+	ref, err := r.Head()
+
+	if err != nil {
+		return err
+	}
+
+	cIter, err := r.Log(&git.LogOptions{From: ref.Hash()})
+
+	if err != nil {
+		return err
+	}
+
+	worktree, err := r.Worktree()
+	if err != nil {
+		return err
+	}
+
+	files := make(chan FileState, runtime.NumCPU())
+
+	cIter.ForEach(func(c *object.Commit) error {
+		res, err := db.Exec("INSERT INTO commits (hash, contributor, author_date, project) VALUES (?, ?, ?, ?)",
+			c.Hash.String(),
+			c.Author.Name,
+			c.Author.When.Unix(),
+			repoUrl,
+		)
+
+		if err != nil && strings.Contains(err.Error(), "Duplicate key") {
+			return nil
+		} else if err != nil {
+			return err
+		}
+
+		commitId, err := res.LastInsertId()
+		if err != nil {
+			return err
+		}
+
+		if err := worktree.Checkout(&git.CheckoutOptions{
+			Hash: c.Hash,
+		}); err != nil {
+			return err
+		}
+
+		processor.ProcessConstants()
+
+		go findFiles(filesystem, commitId, c, files)
+
+		if err = process(filesystem, files); err != nil {
+			return err
+		}
+
+		return nil
+	})
+
+	close(files)
+
+	return nil
+}
+
+func findFiles(filesystem billy.Filesystem, commitId int64, commit *object.Commit, files chan FileState) error {
+	err := util.Walk(filesystem, ".", func(path string, info fs.FileInfo, err error) error {
+		if info.IsDir() {
+			// Not interested in directories
+			return nil
+		}
+		if err != nil {
+			// Also not interested in handling errors passed to the function specifically
+			return nil
+		}
+		f := newFileJob(path, info)
+		if f == nil {
+			return err
+		}
+		files <- FileState{
+			CommitId: commitId,
+			Commit:   commit,
+			FileJob:  f,
+		}
+		return nil
+	})
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+var LargeByteCount int64 = 1000000
+
+func newFileJob(path string, info fs.FileInfo) *processor.FileJob {
+	if info.Size() >= LargeByteCount {
+		return nil
+	}
+
+	if info.Mode()&os.ModeSymlink == os.ModeSymlink {
+		return nil
+	}
+
+	if !info.Mode().IsRegular() {
+		return nil
+	}
+
+	language, extension := processor.DetectLanguage(path)
+
+	if len(language) != 0 {
+		for _, l := range language {
+			processor.LoadLanguageFeature(l)
+		}
+
+		for _, l := range language {
+			if l == "ignore" || l == "gitignore" {
+				return nil
+			}
+		}
+
+		return &processor.FileJob{
+			Location:          path,
+			Filename:          path,
+			Extension:         extension,
+			PossibleLanguages: language,
+			Bytes:             info.Size(),
+			Content:           make([]byte, info.Size()),
+		}
+	} else {
+		fmt.Printf("skipping file unknown extension: %s\n", path)
+	}
+	return nil
+}
+
+func process(filesystem billy.Filesystem, files chan FileState) error {
+	var wg sync.WaitGroup
+
+	for i := 0; i < runtime.NumCPU(); i++ {
+		wg.Add(1)
+		go func() {
+			for file := range files {
+				loc := file.Location
+				f, err := filesystem.Open(loc)
+				if err != nil {
+					fmt.Println("error opening file:", err)
+					continue
+				}
+				_, err = f.Read(file.Content)
+				if err != nil {
+					fmt.Println("error reading file:", err)
+					continue
+				}
+				processFile(file)
+			}
+			wg.Done()
+		}()
+	}
+
+	wg.Wait()
+
+	return nil
+}
+
+func processFile(file FileState) {
+	file.Language = processor.DetermineLanguage(file.Filename, file.Language, file.PossibleLanguages, file.Content)
+	if file.Language == processor.SheBang {
+
+		cutoff := 200
+
+		// To avoid runtime panic check if the content we are cutting is smaller than 200
+		if len(file.Content) < cutoff {
+			cutoff = len(file.Content)
+		}
+
+		lang, err := processor.DetectSheBang(string(file.Content[:cutoff]))
+		if err != nil {
+			fmt.Println("error detecting shebang:", err)
+			return
+		}
+
+		file.Language = lang
+		processor.LoadLanguageFeature(lang)
+	}
+
+	processor.CountStats(file.FileJob)
+
+	if file.Binary {
+		fmt.Println("skipping file as binary:", file.Location)
+		return
+	}
+
+	// TODO: add to duckdb
+	// TODO: Could improve performance by configuring gc
+}
